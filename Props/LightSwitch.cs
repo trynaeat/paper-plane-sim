96b84c53855335d231ab635371936a7c3fd724bd
trynaeat <jamesparks10@gmail.com>
2024-09-04 17:31:25 -0400

    Abstracted switch

--- Props/LightSwitch.cs
+++ Props/LightSwitch.cs
@@ -1,39 +1,20 @@
 using Godot;
 
-public partial class LightSwitch : Node3D
+public partial class LightSwitch : Switch
 {
-	// Sets starting state
-	[Export]
-	public bool On = false;
-	[Export]
-	public int switchChannel;
-	[Signal]
-	public delegate void SwitchedEventHandler(int switchChannel, bool state);
-	private bool _on = false;
-	private bool _waitDebounce = false;
 	private Node3D _pivot;
-	private AudioStreamPlayer3D _audio;
 
-	private AudioStream _soundHigh;
-	private AudioStream _soundLow;
-
-	private Timer _hitTimer;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this._on = On;
+		base._Ready();
 		this._pivot = GetNode<Node3D>("Pivot");
-		this._hitTimer = GetNode<Timer>("HitTimer");
 		if (this._on) {
 			this._pivot.RotateX(Mathf.DegToRad(-31));
 		} else
 		{
 			this._pivot.RotateX(Mathf.DegToRad(31));
 		}
-		this._audio = GetNode<AudioStreamPlayer3D>("AudioStream");
-		this._soundHigh = GD.Load<AudioStream>("res://Sounds/Light_Click.wav");
-		this._soundLow = GD.Load<AudioStream>("res://Sounds/Light_Click_Low.wav");
-		EmitSignal(SignalName.Switched, switchChannel, _on);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,37 +22,17 @@ public partial class LightSwitch : Node3D
 	{
 	}
 
-	public void OnAreaEntered(Area3D area)
+	protected override void _OnFlip()
 	{
-		if(!_waitDebounce)
-		{
-			_flipSwitch();
-		}
-	}
-
-	public void OnHitTimerTimeout()
-	{
-		this._waitDebounce = false;
-	}
-
-	private void _flipSwitch ()
-	{
-		this._waitDebounce = true;
-		_hitTimer.Start();
 		Vector3 targetRotation;
-		this._on = !this._on;
 		if (this._on)
 		{
-			this._audio.Stream = this._soundHigh;
 			targetRotation = new Vector3(-31, 0, 0);
 		}
 		else
 		{
-			this._audio.Stream = this._soundLow;
 			targetRotation = new Vector3(31, 0, 0);
 		}
 		this._pivot.RotationDegrees = targetRotation;
-		EmitSignal(SignalName.Switched, switchChannel, _on);
-		this._audio.Play();
 	}
 }
---
+++ Props/Switch.cs
@@ -0,0 +1,72 @@
+using Godot;
+
+public abstract partial class Switch : Node3D
+{
+	// Sets starting state
+	[Export]
+	public bool On = false;
+	[Export]
+	public int switchChannel;
+	[Export]
+	public AudioStream SoundHigh = null;
+	[Export]
+	public AudioStream SoundLow = null;
+	[Signal]
+	public delegate void SwitchedEventHandler(int switchChannel, bool state);
+	protected bool _on = false;
+	private bool _waitDebounce = false;
+	private Timer _hitTimer;
+	private AudioStreamPlayer3D _audio;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this._on = On;
+		this._hitTimer = GetNode<Timer>("HitTimer");
+		if (SoundHigh != null || SoundLow != null)
+		{
+			this._audio = GetNode<AudioStreamPlayer3D>("AudioStream");
+		}
+		EmitSignal(SignalName.Switched, switchChannel, _on);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnAreaEntered(Area3D area)
+	{
+		if(!_waitDebounce)
+		{
+			_flipSwitch();
+		}
+	}
+
+	public void OnHitTimerTimeout()
+	{
+		this._waitDebounce = false;
+	}
+
+	protected abstract void _OnFlip();
+
+	private void _flipSwitch()
+	{
+		this._on = !this._on;
+		this._waitDebounce = true;
+		_hitTimer.Start();
+		if (this._on && this._audio != null)
+		{
+			this._audio.Stream = SoundHigh;
+		} else if (this._audio != null)
+		{
+			this._audio.Stream = SoundLow;
+		}
+		if (this._audio != null && this._audio.Stream != null)
+		{
+			GD.Print("Hello!");
+			this._audio.Play();
+		}
+		EmitSignal(SignalName.Switched, switchChannel, _on);
+		_OnFlip();
+	}
+}
